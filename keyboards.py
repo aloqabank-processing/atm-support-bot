a5a45bb1c4dcd49c37192fd76d416091935c9d94
hetredtt <125869909+hetredtt@users.noreply.github.com>
2023-09-15 18:05:11 +0500

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -9,6 +9,11 @@ def share_keyboard():
     keyboard.add(button)
     return keyboard
 
+def send_location(text_for_location):
+    keyboard = ReplyKeyboardMarkup()
+    button = KeyboardButton(text_for_location, request_location=True)
+    keyboard.add(button)
+    return keyboard
 
 def choose_language():
     buttons = [
@@ -43,6 +48,26 @@ def no_photo(text_for_add, text_for_enter):
     buttons = [
         InlineKeyboardButton(text=text_for_add, callback_data="noPhoto"),
         InlineKeyboardButton(text=text_for_enter, callback_data="enter"),
+    ]
+    keyboard = InlineKeyboardMarkup(row_width=1)
+    keyboard.add(*buttons)
+    return keyboard
+
+def choose_problem(text_for_card_reissue, text_for_ATM, text_for_tickets):
+    buttons = [
+        InlineKeyboardButton(text=text_for_card_reissue, callback_data="card_reissue"),
+        InlineKeyboardButton(text=text_for_ATM, callback_data="ATM"),
+        InlineKeyboardButton(text=text_for_tickets, callback_data="tickets"),
+    ]
+    keyboard = InlineKeyboardMarkup(row_width=1)
+    keyboard.add(*buttons)
+    return keyboard
+
+def method_to_choose_ATM(text_for_location, text_for_QR, text_for_back_from_choose_ATM):
+    buttons = [
+        InlineKeyboardButton(text=text_for_location, callback_data="location"),
+        InlineKeyboardButton(text=text_for_QR, callback_data="QR"),
+        InlineKeyboardButton(text=text_for_back_from_choose_ATM, callback_data="back_from_choose_ATM"),
     ]
     keyboard = InlineKeyboardMarkup(row_width=1)
     keyboard.add(*buttons)
