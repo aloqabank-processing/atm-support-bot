014eaf895b4f89a51c3ba238df5bd46a085db69e
hetredtt <125869909+hetredtt@users.noreply.github.com>
2023-09-27 12:48:34 +0500

    Update main_menu.py

--- services/main_menu.py
+++ services/main_menu.py
@@ -20,7 +20,9 @@ async def main_menu(call, state):
         await bot.send_message(chat_id=call.message.chat.id, text=language['2'], reply_markup=keyboard.add_comp(language['5'], language['11'], language['14']))
     elif call.data == "cancellation_the_transaction":
         await bot.send_message(chat_id=call.message.chat.id, text=language['37'], reply_markup=keyboard.cancellation_the_transaction_type())
-    elif call.data == "tickets":
+    elif call.data == "atm_repair":
+        await bot.send_message(chat_id=call.message.chat.id, text=language['40'], reply_markup=keyboard.filial_list(language['24']))
+    else:
         user_id = call.message.chat.id
         result = ticket.select_ticket_card_reissue(user_id)
         ticket_num_counter = 0
@@ -38,7 +40,7 @@ async def main_menu(call, state):
             await bot.send_message(chat_id=call.message.chat.id, text=language['31'], reply_markup=keyboard.ticket_list( language['24'], result, result_atm_ticket ))
         else:
             await bot.send_message(chat_id=call.message.chat.id, text=language['32'], reply_markup=keyboard.ticket_list( language['24'], result, result_atm_ticket ))
-
+    
 async def ticket_menu(call, state):
     number = int(call.data)
 
@@ -52,7 +54,7 @@ async def ticket_menu(call, state):
 
     if len(result) != 0:
         result_of_selection = result[0]
-        if str(result_of_selection[5]) == 'c' or str(result_of_selection[5]) == 'UZCARD':
+        if str(result_of_selection[5]) == 'card_reissue' or str(result_of_selection[5]) == 'UZCARD':
             client_ticket_form = language['28'] + str(number) + "\n" + result_of_selection[2] + "\n" + language['29'] + result_of_selection[3] + "\n" + language['30'] + result_of_selection[4]
 
             msg = await bot.send_message(chat_id=call.message.chat.id, text=client_ticket_form, reply_markup=keyboard.back_to_choose_ATM(language['24']))
@@ -61,7 +63,6 @@ async def ticket_menu(call, state):
             client_ticket_form = language['28'] + str(number) + "\n" + language['29'] + result_of_selection[3] + "\n" + language['30'] + result_of_selection[4]
 
             await bot.send_document(chat_id=call.message.chat.id, document=str(result_of_selection[2]), caption=client_ticket_form, reply_markup=keyboard.back_to_choose_ATM(language['24']))
-
 
     if len(atm_result) != 0:
         result_of_selection = atm_result[0]
@@ -84,7 +85,7 @@ async def next(FILIAL, call, state):
         await bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text = language['4'] )
 
     if str(call.message.chat.id) == FILIAL:
-        msg = await bot.send_message(chat_id=call.message.chat.id, text=language['20'], reply_markup=keyboard.choose_problem(language['18'], language['17'], language['19'], language['34']))
+        msg = await bot.send_message(chat_id=call.message.chat.id, text=language['20'], reply_markup=keyboard.choose_problem(language['18'], language['17'], language['19'], language['34'], language['39']))
         await state.update_data(button=msg.message_id)
     else:
         msg = await bot.send_message(chat_id=call.message.chat.id, text=language['20'], reply_markup=keyboard.choose_problem_user(language['18'], language['17'], language['19']))
