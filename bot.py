00eea082e2d50bc2e865855fc8fcec55e1f3522b
hetredtt <125869909+hetredtt@users.noreply.github.com>
2023-09-28 17:25:41 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,7 +32,7 @@ logging.basicConfig(level=logging.INFO)
 atm_model_array = ["C2040", "C2060", "C2070", "C4060", "DN200H", "GRG H22", "GRG H68NL", "GRG H68VL", "GRG P5800VL", "NCR 6622", "NCR 6623", "NCR 6682"]
 # List of terminal id
 terminal_id_array = atm_repair.just_to_get_terminal_id_list()
-
+print(terminal_id_array)
 # # # # # # # # # # # # # # # # # #
 # Registration and Auth
 # # # # # # # # # # # # # # # # # #
@@ -53,6 +53,45 @@ async def get_contact(message: Message, state: FSMContext):
     await language_select.registation(GROUP_ID, message, state)
 
 # # # # # # # # # # # # # # # # # #
+# ATM repair
+# # # # # # # # # # # # # # # # # #
+# Getting ATM-model by state
+@dp.callback_query_handler(text=["Amaliyot", "Qoraqalpogiston", "Xorazm", "Namangan", "Andijon", "Buxoro", "Surxondaryo", "Samarqand", "Qashqadaryo", "Navoiy", "Fargona", "Jizzax", "Qoqon"], state='*')
+async def model_by_filial(call: CallbackQuery, state: FSMContext):
+    await call.answer('Done')
+    await atm_repair.model_by_filial(call, state)
+# Getting terminal ID by ATM-model
+@dp.callback_query_handler(text=atm_model_array, state='*')
+async def terminal_id_by_model(call: CallbackQuery, state: FSMContext):
+    await call.answer('Done')
+    await atm_repair.terminal_id_by_model(call, state)
+# Asking for a form by terminal id
+@dp.callback_query_handler(text=terminal_id_array, state='*')
+async def ask_form(call: CallbackQuery, state: FSMContext):
+    await call.answer('Done')
+    print('ask form')
+    await atm_repair.ask_form(call, state)
+    await UserStates.atm_repair_get_form.set()
+# Filling form for ATM repair ticket
+@dp.message_handler(state=UserStates.atm_repair_get_form)
+async def get_form(message: Message, state: FSMContext):
+    print("atm_repair_get_form")
+    await atm_repair.get_form(GROUP_ID, message, state)
+# Something like admin panel in admin group to operate with tickets
+@dp.callback_query_handler(text=["answer_ticket_atm_repair", "status_ticket_atm_repair", "close_ticket_atm_repair"], state='*')
+async def admin_operations(call: CallbackQuery, state: FSMContext):
+    await call.answer('Done')
+    await atm_repair.admin_operations(GROUP_ID, call, state)
+# Action to change status
+@dp.message_handler(chat_id=GROUP_ID, state=UserStates.admin_group_status_atm_repair)
+async def change_status(message: Message, state: FSMContext):
+    await atm_repair.change_status(GROUP_ID, message, state)
+# Action to change answer
+@dp.message_handler(chat_id=GROUP_ID, state=UserStates.admin_group_answer_atm_repair)
+async def change_answer(message: Message, state: FSMContext):
+    await atm_repair.change_answer(GROUP_ID, message, state)
+
+# # # # # # # # # # # # # # # # # #
 # Main menu
 # # # # # # # # # # # # # # # # # #
 # Choose problem or ticket 
@@ -70,42 +109,6 @@ async def ticket_menu(call: CallbackQuery, state: FSMContext):
 async def next(call: CallbackQuery, state: FSMContext):
     await call.answer('Done')
     await main_menu.next(FILIAL, call, state)
-
-# # # # # # # # # # # # # # # # # #
-# ATM repair
-# # # # # # # # # # # # # # # # # #
-# Getting ATM-model by state
-@dp.callback_query_handler(text=["Amaliyot", "Qoraqalpogiston", "Xorazm", "Namangan", "Andijon", "Buxoro", "Surxondaryo", "Samarqand", "Qashqadaryo", "Navoiy", "Fargona", "Jizzax", "Qoqon"], state='*')
-async def model_by_filial(call: CallbackQuery, state: FSMContext):
-    await call.answer('Done')
-    await atm_repair.model_by_filial(call, state)
-# Getting terminal ID by ATM-model
-@dp.callback_query_handler(text=atm_model_array, state='*')
-async def terminal_id_by_model(call: CallbackQuery, state: FSMContext):
-    await call.answer('Done')
-    await atm_repair.terminal_id_by_model(call, state)
-# Asking for a form by terminal id
-@dp.callback_query_handler(text=terminal_id_array, state='*')
-async def ask_form(call: CallbackQuery, state: FSMContext):
-    await call.answer('Done')
-    await atm_repair.ask_form(call, state)
-# Filling form for ATM repair ticket
-@dp.message_handler(content_types=ContentType.TEXT, state=UserStates.atm_repair_get_form)
-async def get_form(message: Message, state: FSMContext):
-    await atm_repair.get_form(GROUP_ID, message, state)
-# Something like admin panel in admin group to operate with tickets
-@dp.callback_query_handler(text=["answer_ticket_atm_repair", "status_ticket_atm_repair", "close_ticket_atm_repair"], state='*')
-async def admin_operations(call: CallbackQuery, state: FSMContext):
-    await call.answer('Done')
-    await atm_repair.admin_operations(GROUP_ID, call, state)
-# Action to change status
-@dp.message_handler(chat_id=GROUP_ID, state=UserStates.admin_group_status_cancellation_the_transaction)
-async def change_status(message: Message, state: FSMContext):
-    await atm_repair.change_status(GROUP_ID, message, state)
-# Action to change answer
-@dp.message_handler(chat_id=GROUP_ID, state=UserStates.admin_group_answer_cancellation_the_transaction)
-async def change_answer(message: Message, state: FSMContext):
-    await atm_repair.change_answer(GROUP_ID, message, state)
 
 # # # # # # # # # # # # # # # # # #
 # Cancellation The Transaction
