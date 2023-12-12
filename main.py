a3278f07e1d7ddb31a7357e0a2b5149eb3d47411
hetredtt <125869909+hetredtt@users.noreply.github.com>
2023-12-12 20:50:33 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from aiogram import executor
 from dispatcher import dp
-import bot as cmd
+import controllers.atm_repair, controllers.cancel, controllers.card_reissue, controllers.feedback, controllers.menu_controller as cmd
+# import bot as cmd
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
