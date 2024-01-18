c6d4077804409f4f1e33c6349f9099d37763808c
hetredtt <125869909+hetredtt@users.noreply.github.com>
2024-01-18 18:52:43 +0500

    Update card_reissue.py

--- services/methods/card_reissue.py
+++ services/methods/card_reissue.py
@@ -4,6 +4,7 @@ from dispatcher import bot
 import keyboards as keyboard
 from datetime import datetime
 from states import UserStates
+from api_query import FeedbackModule
 
 config_file = 'config.ini'
 db = Database(config_file)
@@ -21,19 +22,6 @@ async def get_form(GROUP_ID, message, state):
     user_id = message.chat.id
     ticket_id = ticket.get_last_num_card_reissue()
     ticket_id = int(ticket_id) + 1
-
-    url = "https://185.217.131.28:7000/feedback/"
-
-    # feedback_data = {
-    #     "type": "TRANSFER",
-    #     "user_id": str(user_id),
-    #     "client_form": com,
-    #     "category": "Другое",
-    #     "device_uid": '-' if exist_photo == 0 else str(serial_num),
-    # }
-
-    # async with httpx.AsyncClient() as client:
-    #     response = await client.post(url, json=feedback_data)
 
     ticket.add_ticket_card_reissue(user_id, card_reissue_ticket, 'card_reissue')
     card_reissue_ticket = "Заявка на перевод карты от " + '<b>' + str(uinfoCut[0]) + '</b> \nФорма составленная клиентом: \n------------------\n' + card_reissue_ticket + '\n------------------\n' + "Номер телефона: <b>(" + uinfoCut[1] + ") </b>"
@@ -54,6 +42,7 @@ async def admin_operations(GROUP_ID, call, state):
     await state.update_data(current_ticket=split_ticket_id)
     if call.data == "close_ticket_card_reissue":
         ticket.delete_ticket_card_reissue(split_ticket_id)
+        await FeedbackModule.update_status(feedback_id=split_ticket_id, status="StatusType.CLOSED")
         await bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
         await bot.send_message(chat_id=GROUP_ID, text="Тикет №" + str(split_ticket_id) + " закрыт")
     elif call.data == "status_ticket_card_reissue":
@@ -75,7 +64,7 @@ async def change_status(GROUP_ID, message, state):
     current_ticket = temp_data.get('current_ticket')
     current_ticket_text = temp_data.get('current_ticket_text')
 
-    ticket.update_status_by_id(status_by_admin, current_ticket)
+    await FeedbackModule.update_status(feedback_id=current_ticket, status=status_by_admin)
 
     await bot.send_message(chat_id=GROUP_ID, text="Статус заявки успешно обновлен!")
     await bot.send_message(chat_id=GROUP_ID, text=current_ticket_text, reply_markup=keyboard.options_ticket_card_reissue() )
@@ -87,7 +76,7 @@ async def change_answer(GROUP_ID, message, state):
     current_ticket = temp_data.get('current_ticket')
     current_ticket_text = temp_data.get('current_ticket_text')
 
-    ticket.update_answer_by_id(answer_by_admin, current_ticket)
+    await FeedbackModule.update_answer(answer_by_admin, current_ticket)
 
     await bot.send_message(chat_id=GROUP_ID, text="Ответ клиенту успешно добавлен!")
     await bot.send_message(chat_id=GROUP_ID, text=current_ticket_text, reply_markup=keyboard.options_ticket_card_reissue() )
