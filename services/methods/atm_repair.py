2cf0e3e0b5042d2821fa0baaee0b0f42639a6418
hetredtt <125869909+hetredtt@users.noreply.github.com>
2024-01-22 15:53:27 +0500

    Update atm_repair.py

--- services/methods/atm_repair.py
+++ services/methods/atm_repair.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import keyboards as keyboard
 from repository import Atm, Ticket, User
 from db import Database
-from api_query import FeedbackModule
+from api_query import FeedbackModule, MonitoringModel, AdministrationModule
 
 config_file = 'config.ini'
 db = Database(config_file)
@@ -18,10 +18,10 @@ def just_to_get_terminal_id_list():
 
 async def model_by_filial(call, state):
     await state.update_data(atm_state=call.data)
-    result = atm.get_model_by_filial( call.data )
+    result = await MonitoringModel.get_model_by_state(call.data)
     model = []
     for temp_model in result:
-        model.append(temp_model[0])
+        model.append(temp_model['Model'])
     model = set(model)
     temp_data = await state.get_data()
     language = temp_data.get('language')
@@ -32,7 +32,7 @@ async def terminal_id_by_model(call, state):
     temp_data = await state.get_data()
     language = temp_data.get('language')
     atm_state = temp_data.get('atm_state')
-    terminal_id = atm.get_terminal_id_by_model(atm_state, call.data)
+    terminal_id = await MonitoringModel.get_terminal_id(atm_state, call.data)
     await bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
     await bot.send_message(chat_id=call.message.chat.id, text=language['42'], reply_markup=keyboard.terminal_id_list(terminal_id, language['24']))
 
@@ -48,7 +48,7 @@ async def ask_form(call, state):
     print(current_state)
 
 async def get_form(GROUP_ID, message, state):
-    uinfo = user.info(message.from_user.id)
+    uinfo = await AdministrationModule.get_user(message.from_user.id)
     uinfoCut = uinfo[0]
     temp_data = await state.get_data()
     language = temp_data.get('language')
@@ -56,12 +56,11 @@ async def get_form(GROUP_ID, message, state):
     terminal_id_for_atm_repair = temp_data.get('terminal_id_for_atm_repair')
     atm_repair_ticket = message.text.replace("'", "''")
     user_id = message.chat.id
-    ticket_id = ticket.get_last_num_card_reissue()
-    ticket_id = int(ticket_id) + 1
+    ticket_id = await FeedbackModule.get_feedback_id_last(user_id)
 
     ticket.add_ticket_card_reissue(user_id, atm_repair_ticket, terminal_id_for_atm_repair)
-    ticket_form = "Заявка на починку банкомата от " + '<b>' + str(uinfoCut[0]) + '\n' + "Регион: " + str(atm_state) + "\n" + "Terminal ID: " + str(terminal_id_for_atm_repair) + '</b>\nФорма составленная клиентом: \n------------------\n' + str(atm_repair_ticket) + '\n------------------\n' + "Номер телефона: <b>(" + uinfoCut[1] + ") </b>"
-    ticket_form = str(ticket_id) + '\n' + ticket_form
+    ticket_form = "Заявка на починку банкомата от " + '<b>' + str(uinfoCut['name']) + '\n' + "Регион: " + str(atm_state) + "\n" + "Terminal ID: " + str(terminal_id_for_atm_repair) + '</b>\nФорма составленная клиентом: \n------------------\n' + str(atm_repair_ticket) + '\n------------------\n' + "Номер телефона: <b>(" + uinfoCut['mobile'] + ") </b>"
+    ticket_form = str(ticket_id['feedback_id']) + '\n' + ticket_form
     await bot.send_message(chat_id=GROUP_ID, text=ticket_form, reply_markup=keyboard.options_ticket_atm_repair())
 
     current_hour = datetime.now().hour
